a0567a4b229175743f424416a95a782b1eb449cd
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-05-02 12:21:48 +0200

    Add Getter and Setter

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -14,6 +14,78 @@ class Article implements JsonSerializable
     private string $content;
     private int $categorie_id;
 
+    /*
+     * Get the value of id
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /*
+     * Set the value of id
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /*
+     * Get the value of title
+     */
+    public function getTitle(): String
+    {
+        return $this->title;
+    }
+
+    /*
+     * Set the value of title
+     */
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /*
+     * Get the value of content
+     */
+    public function getContent(): String
+    {
+        return $this->content;
+    }
+
+    /*
+     * Set the value of content
+     */
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /*
+     * Get the value of categorie_id
+     */
+    public function getCategorieId(): int
+    {
+        return $this->categorie_id;
+    }
+
+    /*
+     * Set the value of categorie_id
+     */
+    public function setCategorieId(int $categorie_id): self
+    {
+        $this->categorie_id = $categorie_id;
+
+        return $this;
+    }
+
     public function jsonSerialize(): mixed
     {
         return [
