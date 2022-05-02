c1daf55bd8f6a7c9a1ed19903836b89c0360e151
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-05-02 12:39:37 +0200

    Creating basic router

--- public/index.php
+++ public/index.php
@@ -8,5 +8,23 @@ require ROOT . "/vendor/autoload.php";
 
 // (new CategorieControlleur)->index();
 // (new ArticleControlleur)->index();
-(new CategorieControlleur)->single(1);
+// (new CategorieControlleur)->single(1);
 // (new ArticleControlleur)->single(1);
+
+if (isset($_SERVER["PATH_INFO"])) {
+    $path = explode("/", $_SERVER["PATH_INFO"]);
+    if (isset($path[3])) {
+        $controlleurName = "App\Controlleur\\" . ucfirst($path[3]) . "Controlleur";
+        $controlleur = new $controlleurName();
+        if (isset($path[4]) && is_numeric($path[4])) {
+            $controlleur->single($path[4]);
+        } elseif (isset($path[4]) && is_string($path[4])) {
+            $method = $path[4];
+            $controlleur->$method();
+        } else {
+            $controlleur->index();
+        }
+    }
+} else {
+    echo "tg_enkul";
+}
