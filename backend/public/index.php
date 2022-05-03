e82255f0f7e8c1cd92d027e0429badba43b21c3e
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-05-03 11:13:45 +0200

    move to backend

--- backend/public/index.php
+++ backend/public/index.php
@@ -6,4 +6,7 @@ define("ROOT", dirname(__DIR__));
 require ROOT . "/vendor/autoload.php";
 require ROOT . "/Core/Routeur/Routeur.php";
 
+// header("Access-Control-Allow-Origin: http://127.0.0.1:8080");
+header("Access-Control-Allow-Origin: *");
+
 Routeur::routeur();
