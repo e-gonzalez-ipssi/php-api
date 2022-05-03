7b7c4ca755dd8de23adc4d13847fa3716d1d9139
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-05-03 09:46:25 +0200

    Update router

--- public/index.php
+++ public/index.php
@@ -13,19 +13,56 @@ require ROOT . "/vendor/autoload.php";
 
 if (isset($_SERVER["PATH_INFO"])) {
     $path = explode("/", $_SERVER["PATH_INFO"]);
-    if (isset($path[3])) {
-        $controlleurName = "App\Controlleur\\" . ucfirst($path[3]) . "Controlleur";
+
+    if (isset($path[1])) {
+        $controlleurName = "App\Controlleur\\" . ucfirst($path[1]) . "Controlleur";
         $controlleur = new $controlleurName();
 
-        if (isset($path[4]) && is_numeric($path[4])) {
-            $controlleur->single($path[4]);
-        } elseif (isset($path[4]) && is_string($path[4])) {
-            $method = $path[4];
-            $controlleur->$method();
-        } else {
-            $controlleur->index();
+        $id = null;
+        if (isset($path[2]) && is_numeric($path[2])) {
+            $id = $path[2];
         }
+
+        switch ($_SERVER["REQUEST_METHOD"]) {
+            case 'GET':
+                if ($id) {
+                    $controlleur->single($id);
+                } else {
+                    $controlleur->index();
+                }
+                break;
+            case 'POST':
+                if (!empty($_POST)) {
+                    $controlleur->save($_POST);
+                } else {
+                    throw new Exception("Donné manquante pour l'ajout en BDD", 400);
+                }
+                break;
+            case 'PUT':
+                parse_str(file_get_contents("php://input"), $_PUT);
+                if ($id && !empty($_PUT)) {
+                    $controlleur->update($id, $_PUT);
+                } else {
+                    throw new Exception("Erreur lors de la modification, il mannque des informations", 400);
+                }
+                break;
+            case 'PATCH':
+                parse_str(file_get_contents("php://input"), $_PATCH);
+                if ($id && !empty($_PATCH)) {
+                    $controlleur->update($id, $_PATCH);
+                } else {
+                    throw new Exception("Erreur lors de la modification, il mannque des informations", 400);
+                }
+                break;
+            case 'DELETE':
+                if ($id) {
+                    $controlleur->delete($id);
+                } else {
+                    throw new Exception("ID manquant", 400);
+                }
+                break;
+        };
     }
 } else {
-    echo "tg_enkul";
+    throw new Exception("No route matched");
 }
